2be4eebb959ab5b04aeb06b122783a76bea6b79b
Aubin Daffa <kyodirman@gmail.com>
2024-02-28 22:36:55 +0700

    Tugas Pekan 1

---
+++ Praktikum 2/proses_nilai.php
@@ -0,0 +1,14 @@
+<?php
+// tangkap input form
+$nama = $_GET['nama'];
+$matkul = $_GET['matkul'];
+$uts = $_GET['uts'];
+$uas = $_GET['uas'];
+$tugas = $_GET['tugas'];
+
+//tampilan hasil input
+echo "<br> Nama Lengkap: $nama";
+echo "<br> Mata Kuliah: $matkul";
+echo "<br> Nilai UTS: $uts";
+echo "<br> Nilai UAS: $uas";
+echo "<br> Nilai Tugas: $tugas";
